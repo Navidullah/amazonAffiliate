21fc86b36077c456cc6a750142a26c85e91c6e70
komal ,k c~x <komaalfarid@gmail.com>
2025-08-03 08:47:49 +0500

    sitemape added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.shopyor.com", // replace with your domain
+  generateRobotsTxt: true,
+  // ...you can add more options, like exclude or additional paths
+};
