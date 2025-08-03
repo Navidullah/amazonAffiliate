747fa29152d362f6cd76e6949fed721f8fffb70f
komal ,k c~x <komaalfarid@gmail.com>
2025-08-03 09:56:23 +0500

    blogs removed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   siteUrl: "https://www.shopyor.com", // replace with your domain
   generateRobotsTxt: true,
+
   // ...you can add more options, like exclude or additional paths
 };
