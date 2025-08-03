7181516672f45755777d7f701d86656434807d91
komal ,k c~x <komaalfarid@gmail.com>
2025-08-03 11:17:37 +0500

    added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,34 @@
 // next-sitemap.config.js (ES module syntax)
+
+const siteUrl = "https://www.shopyor.com";
+
 export default {
-  siteUrl: "https://www.shopyor.com",
+  siteUrl,
   generateRobotsTxt: true,
   exclude: [],
   additionalPaths: async (config) => {
+    const res = await fetch(`${siteUrl}/api/blogs`);
+    const blogs = await res.json();
+
+    // Build blog URLs for sitemap
+    const blogPaths = blogs.map((blog) => ({
+      loc: `${siteUrl}/blogs/${blog.slug}`,
+      lastmod: new Date(
+        blog.updatedAt || blog.date || new Date()
+      ).toISOString(),
+      changefreq: "weekly",
+      priority: 0.7,
+    }));
+
+    // Also include /blogs listing page manually
     return [
       {
-        loc: "https://www.shopyor.com/blogs",
+        loc: `${siteUrl}/blogs`,
         changefreq: "daily",
         priority: 0.7,
         lastmod: new Date().toISOString(),
       },
+      ...blogPaths,
     ];
   },
 };
