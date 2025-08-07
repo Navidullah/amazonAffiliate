83fc4742a6972714907b4b95f00a9a3cddcb7164
komal ,k c~x <komaalfarid@gmail.com>
2025-08-07 17:21:38 +0500

    auto

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -32,7 +32,7 @@ export default async function BlogHomePage() {
   }));
 
   return (
-    <main className="mainwrapper pt-24 px-4 sm:px-6 ">
+    <main className=" pt-24 px-4 sm:px-6 ">
       <BlogHero className="mb-8" />
 
       <BlogList blogs={blogsWithCounts} />
