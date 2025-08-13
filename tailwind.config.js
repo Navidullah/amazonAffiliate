e9298cee7fc6e1aa09bc8f0f39bf4f67a2cb685f
komal ,k c~x <komaalfarid@gmail.com>
2025-08-13 16:03:36 +0500

    ass

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         geist: ["var(--font-geist-sans)"],
         geistMono: ["var(--font-geist-mono)"],
       },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(90deg, #a78bfa, #8498fa, #60a5fa, #a38ef4, #f472b6, #c07dfa, #a78bfa)",
+      },
     },
   },
   plugins: [
